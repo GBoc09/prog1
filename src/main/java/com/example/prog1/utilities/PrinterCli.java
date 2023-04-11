defd9593c93f69399ac57dd5a58d696a72728e51
giulia <giuliab0909@gmail.com>
2023-04-11 10:46:05 +0200

    42 -- PrinterCli correction

--- src/main/java/com/example/prog1/utilities/PrinterCli.java
+++ src/main/java/com/example/prog1/utilities/PrinterCli.java
@@ -1,6 +1,9 @@
 package com.example.prog1.utilities;
 
 public class PrinterCli {
+    private PrinterCli() {
+        throw new IllegalStateException("Utility class");
+    }
     public static void printMessage(String s){
         System.out.println(s);
     }
